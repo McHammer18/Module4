cc31f316ff852cfa8c834f92a1d59c649948ba3b
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-09-21 16:14:03 -0500

    Test for under fifty created but does not pass

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -25,5 +25,16 @@ class MyTestCase(unittest.TestCase):
         test_price_under_thirty.assertEqual(24.91, coupon_calculations.calculate_order(30, 10, .20))
         test_price_under_thirty.assertEqual(23.85, coupon_calculations.calculate_order(30, 10, .25))
         test_price_under_thirty.assertEqual(22.79, coupon_calculations.calculate_order(30, 10, .30))
+    def test_under_50(test_price_under_fifty):
+        test_price_under_fifty.assertEqual(31.80, coupon_calculations.calculate_order(50, 5, .10))
+        test_price_under_fifty.assertEqual(30.47, coupon_calculations.calculate_order(30, 5, .15))
+        test_price_under_fifty.assertEqual(29.15, coupon_calculations.calculate_order(30, 5, .20))
+        test_price_under_fifty.assertEqual(27.82, coupon_calculations.calculate_order(30, 5, .25))
+        test_price_under_fifty.assertEqual(26.50, coupon_calculations.calculate_order(30, 5, .30))
+        test_price_under_fifty.assertEqual(27.03, coupon_calculations.calculate_order(30, 10, .10))
+        test_price_under_fifty.assertEqual(25.97, coupon_calculations.calculate_order(30, 10, .15))
+        test_price_under_fifty.assertEqual(24.91, coupon_calculations.calculate_order(30, 10, .20))
+        test_price_under_fifty.assertEqual(23.85, coupon_calculations.calculate_order(30, 10, .25))
+        test_price_under_fifty.assertEqual(22.79, coupon_calculations.calculate_order(30, 10, .30))
 if __name__ == '__main__':
     unittest.main()
