1a548aa8a3a6e5f81851e466e0d22b8d82e5dac1
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-09-21 15:59:21 -0500

    Test for under thirty and its combintions built but fails

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -14,5 +14,16 @@ class MyTestCase(unittest.TestCase):
         test_price_under_ten.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .25))
         test_price_under_ten.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .25))
         test_price_under_ten.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .30))
+    def test_under_30(test_price_under_thirty):
+        test_price_under_thirty.assertEqual(10.72, coupon_calculations.calculate_order(20, 5, .10))
+        test_price_under_thirty.assertEqual(10.46, coupon_calculations.calculate_order(10, 5, .15))
+        test_price_under_thirty.assertEqual(10.19, coupon_calculations.calculate_order(10, 5, .20))
+        test_price_under_thirty.assertEqual(9.93, coupon_calculations.calculate_order(10, 5, .25))
+        test_price_under_thirty.assertEqual(9.66, coupon_calculations.calculate_order(10, 5, .30))
+        test_price_under_thirty.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .10))
+        test_price_under_thirty.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .15))
+        test_price_under_thirty.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .25))
+        test_price_under_thirty.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .25))
+        test_price_under_thirty.assertEqual(5.95, coupon_calculations.calculate_order(10, 10, .30))
 if __name__ == '__main__':
     unittest.main()
